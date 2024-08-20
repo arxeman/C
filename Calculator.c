be8d72af0e1f9637620e9244e4ea93e6d794f25e
Aryeman Verma <varyeman@gmail.com>
2024-08-20 18:33:05 +0530

    Calculator.c

--- Calculator.c
+++ Calculator.c
@@ -4,35 +4,33 @@ int main()
     int a,b;
     char choice;
     printf("Enter \n1 for addition \n2 for subtraction \n3 for multiplication \n4 for division\n");
-    scanf("%c",&choice);
-    printf("Enter two numbers: ");
+    scanf("%c",&choice); //choice of user
+    printf("Enter two numbers: ");  //input from user
     scanf("%d %d",&a,&b);
-    
-    //printf("%d",choice);
     if(choice == '1')
     {
-        printf("Sum = %d",a+b);
+        printf("Sum = %d",a+b);  //printing sum
     }
     else if(choice == '2')
     {
         
-        printf("Difference = %d",a-b);
+        printf("Difference = %d",a-b);  //printing difference
     }
     else if(choice == '3')
     {
         double prod;
         prod=a*b;
-        printf("Product = %lf",prod);
+        printf("Product = %lf",prod);  //printing product
     }
     else if(choice == '4')
     {
         double div;
         div=a/b;
-        printf("Division = %lf",div);
+        printf("Quotient = %lf",div);  //printing quotient
     }
     else
     {
-        printf("Wrong choice!");
+        printf("Wrong choice!");  //if number other than 4 is entered
     }
     return 0;
 }
