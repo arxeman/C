242a63a2c80bd1990936144c3330e44cfcce69e0
Aryeman Verma <varyeman@gmail.com>
2024-08-20 18:22:06 +0530

    prime.c

--- prime.c
+++ prime.c
@@ -2,7 +2,7 @@
 int main()
 {
     int n,i,count;
-    count=0;
+    count=0;  //setting counter to 0
     printf("Enter a number: ");
     scanf("%d",&n);  //input from user
     for(i=2;i<=n/2;i++)  //running loop from 2 to n/2 to check divisibilty by them
