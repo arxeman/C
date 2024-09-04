efb64586738e4371bb1bf7adf8209d112dc9e429
Aryeman Verma <varyeman@gmail.com>
2024-09-04 18:50:46 +0530

    Add files via upload

---
+++ exp3_3.c
@@ -0,0 +1,39 @@
+//program to check a number according to user's choice
+//experiment 3.3
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int n,ch;
+    printf("Enter a number: ");
+    scanf("%d",&n);
+    printf("Menu\n1. Check even or odd\n2. Check positive or negative\n3. Find Square\n4. Find square root");
+    printf("\nEnter choice: ");
+    scanf("%d",&ch);
+    switch(ch)
+    {
+        case 1:  //for odd or even check
+        {
+            (n%2==0)?printf("It is an even number"):printf("It is an odd number");
+        }
+        break;
+        case 2:  //for positive or negative test
+        {
+            (n>0)?printf("Positive"):(n<0)?printf("Negative"):printf("Zero");
+        }
+        break;
+        case 3:  //to find square
+        {
+            printf("Square = %d",n*n);
+        }
+        break;
+        case 4:  //to find square root
+        {
+            printf("Square root = %lf",sqrt(n));
+        }
+        break;
+        default:  //when wrong input is given
+            printf("Wrong choice!");
+    }
+    return 0;
+}
