be7756a731ae6aa28bb94121ad5e296940e30d4b
Aryeman Verma <varyeman@gmail.com>
2024-09-09 15:26:19 +0530

    Update exp4_3b.c

--- exp4_3b.c
+++ exp4_3b.c
@@ -23,7 +23,7 @@ int isPrime(int num)  //a number only divisible by itself
 {
     if(num<=1)
         return 0;
-    for(int i=2;i<sqrt(num);i++)  //checking divisibility for prime
+    for(int i=2;i<=num/2;i++)  //checking divisibility for prime
     {
         if(num%i==0)
             return 0;
